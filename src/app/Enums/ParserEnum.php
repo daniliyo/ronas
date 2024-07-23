1daac52b43a1d160dbf2972ff56757ad239a2860
Daniil <daniliyo.p1@gmail.com>
2024-07-23 13:25:31 +0300

    Add api key to .env

--- src/app/Enums/ParserEnum.php
+++ src/app/Enums/ParserEnum.php
@@ -9,16 +9,8 @@ enum ParserEnum:string {
     public function getArgs(array $args){
         
         $result = [];
-        $result['appid'] = "af1d5ebc755fb6a38d24261771c3f729";
+        $result['appid'] = env("OPEN_WEATHER_MAP_API_KEY");
 
-        if($this === ParserEnum::LocationUrl){
-            $result['q'] = $args['q'];
-        } else if($this === ParserEnum::WeatherUrl){
-            $result['lon'] = $args['lat'];
-            $result['lat'] = $args['lon'];
-        }
-
-        return $result;
-
+        return array_merge($args, $result);
     }
 }
